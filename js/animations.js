58ff6689500a2f6b9aef9e925e5b3215260d98f2
Chris Glenn <topherblair@gmail.com>
2015-07-23 20:48:25 -0600

    still working on it

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 	var cCount = 140;
+	var tweetContent;
 
 	//Hide the tweet button and char count
 	$('#tweet-controls').hide();
@@ -19,11 +20,39 @@ $(document).ready(function(){
 	//As the user types in the box, the char count decreases. If it
 	//gets below 130 the text should turn red. If it gets below 0
 	//then the tweet button should be disabled.
-	$('.tweet-compose').on('keydown', function(){
+	$('.tweet-compose').on('keyup', function(){
 		cCount--;
 		$('#char-count').html(cCount);
 		//if cCount is below 130 then turn the text red
-		
+		if(cCount <= 30){
+			$('#char-count').css('color', 'red');
+		}
+		//If count is below 0 then disable the tweet button
+		if(cCount < 0){
+			$('#tweet-submit').prop('disabled', true);
+		}
+		else{
+			$('#tweet-submit').prop('diasbled', false);
+		}
 	})
 
+	//When the user hits the Tweet button, a new tweet is created
+	$('#tweet-submit').click(function(){
+		//take the tweet and assign it to the tweetContent variable
+		tweetContent = $('textarea.tweet-compose').val();
+		//Create the new tweet
+		function newTweet(){
+			var tTweet = "<div class='tweet'><div class='content'><img class='avatar' src='img/alagoon.jpg' /><strong class='fullname'>NAME</strong><p class='tweet-text'></p></div></div>";
+			$('#stream').prepend(tTweet);
+		}
+		newTweet();
+		//Reset the tweet controls and content box
+		$('#tweet-controls').hide();
+		$('.tweet-compose').css('height', '2.5em');
+	})
 })
+
+
+
+
+
